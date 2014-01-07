037da16842248af7fe363c976c3acc1aad3db94a
Adam Pulley <noresponse@ibexuk.com>
2014-01-07 14:13:47 +0000

    Notes added and a while loop in receive handler to avoid overflow

--- dmx-rx.c
+++ dmx-rx.c
@@ -42,117 +42,119 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //********** DMX RECEIVE ***********
 //**********************************
 //**********************************
-void dmx_receive (void)
+void dmx_receive (void)					//<<If you get problems with your PIC not being fast enough to receive DMX (its very fast for some PIC18's) then taking this functions code and putting it directly in the interrupt handler will avoid the call to here and the time this adds for the C compiler related tasks when calling a function
 {
-	BYTE rx_status;
-	BYTE rx_data;
+	static BYTE rx_status;			//static as it can help to avoid the irq use the general ram stack
+	static BYTE rx_data;			//static as it can help to avoid the irq use the general ram stack
 	static WORD dmx_rx_channel_number;
 	static BYTE waiting_for_header = 1;
 	static BYTE waiting_for_break = 1;
 
-
-	//----- GET RECEIVED BYTE -----
-	rx_status = DMX_RX_STATUS_REGISTER;
-	rx_data = DMX_RX_DATA_REGISTER;
-
-	//----- PROCESS RX -----
-	if (rx_status & DMX_RX_OERR_BIT)
+	while (PIE3bits.RC2IE && PIR3bits.RC2IF)		//Keep doing if there is more RX (helps avoid overflow of the buffer)
 	{
-		//-------------------------------
-		//----- OVERRUN HAS OCCURED -----
-		//-------------------------------
-		dmx_rx_reset_port();
-		waiting_for_break = 1;
-	}
-	else if (rx_status & DMX_RX_FERR_BIT)
-	{
-		//FRAMING ERROR - IF DATA IS ZERO THEN ERROR IS A BREAK
-		if (rx_data == 0)
-		{
-			//--------------------------
-			//----- BREAK RECEIVED -----
-			//--------------------------
-			waiting_for_break = 0;
-			waiting_for_header = 1;				//Flag waiting for header
+		//----- GET RECEIVED BYTE -----
+		rx_status = DMX_RX_STATUS_REGISTER;
+		rx_data = DMX_RX_DATA_REGISTER;
 
-			dmx_rx_channel_number = 1;			//Reset the channel counter
-		}
-		else
+		//----- PROCESS RX -----
+		if (rx_status & DMX_RX_OERR_BIT)
 		{
-			//-------------------------------------
-			//----- FRAMING ERROR HAS OCCURED -----
-			//-------------------------------------
+			//-------------------------------
+			//----- OVERRUN HAS OCCURED -----
+			//-------------------------------
 			dmx_rx_reset_port();
 			waiting_for_break = 1;
 		}
-	}
-	else if (waiting_for_header)
-	{
-		//---------------------------
-		//----- HEADER RECEIVED -----
-		//---------------------------
-		if (rx_data == DMX_HEADER_STANDARD)
+		else if (rx_status & DMX_RX_FERR_BIT)
+		{
+			//FRAMING ERROR - IF DATA IS ZERO THEN ERROR IS A BREAK
+			if (rx_data == 0)
+			{
+				//--------------------------
+				//----- BREAK RECEIVED -----
+				//--------------------------
+				waiting_for_break = 0;
+				waiting_for_header = 1;				//Flag waiting for header
+
+				dmx_rx_channel_number = 1;			//Reset the channel counter
+			}
+			else
+			{
+				//-------------------------------------
+				//----- FRAMING ERROR HAS OCCURED -----
+				//-------------------------------------
+				dmx_rx_reset_port();
+				waiting_for_break = 1;
+			}
+		}
+		else if (waiting_for_header)
+		{
+			//---------------------------
+			//----- HEADER RECEIVED -----
+			//---------------------------
+			if (rx_data == DMX_HEADER_STANDARD)
+			{
+				//------------------------------------
+				//----- STANDARD HEADER RECEIVED -----
+				//------------------------------------
+				waiting_for_header = 0;
+			}
+			
+			//<<<<< PUT ANY OTHER HEADER TYPES HERE
+
+			else
+			{
+				//-----------------------------------
+				//----- INVALID HEADER RECEIVED -----
+				//-----------------------------------
+				waiting_for_break = 1;			//Non recognised header - ignore packet
+			}
+		}
+		else if (waiting_for_break)
 		{
 			//------------------------------------
-			//----- STANDARD HEADER RECEIVED -----
+			//----- WE ARE WAITING FOR BREAK -----
 			//------------------------------------
-			waiting_for_header = 0;
-		}
-		
-		//<<<<< PUT ANY OTHER HEADER TYPES HERE
 
+			//Ignore rx until break is received
+
+		}
 		else
 		{
-			//-----------------------------------
-			//----- INVALID HEADER RECEIVED -----
-			//-----------------------------------
-			waiting_for_break = 1;			//Non recognised header - ignore packet
+			//-------------------------
+			//----- DATA RECEIVED -----
+			//-------------------------
+
+			//RESET THE TIMEOUT TIMER
+			dmx_rx_10mS_timeout_timer = DMX_TIMEOUT_10MS_TIME;
+
+			//GET THE DATA BYTE IF IN OUR CHANNEL RANGE
+			if (
+				(dmx_rx_channel_number >= dmx_start_address) &&
+				(dmx_rx_channel_number < (dmx_start_address + DMX_NO_OF_CHANS_TO_RX))
+				)
+			{
+				//THIS CHANNEL IS IN OUR RX WINDOW
+				dmx_rx[dmx_rx_channel_number - dmx_start_address] = rx_data;
+			}
+
+			//FLAG NEW FRAME RECEVIED IF WE HAVE RECEIVED ALL THE CHANNELS WE WANT TO RECEIVE
+			if (dmx_rx_channel_number == (dmx_start_address + DMX_NO_OF_CHANS_TO_RX - 1))
+				dmx_new_frame_received = 1;
+
+			//INCREMENT THE CHANNEL NUMBER READY FOR THE NEXT BYTE
+			dmx_rx_channel_number++;
+			if (dmx_rx_channel_number > 512)
+			{
+				waiting_for_break = 1;
+			}
+
 		}
-	}
-	else if (waiting_for_break)
-	{
-		//------------------------------------
-		//----- WE ARE WAITING FOR BREAK -----
-		//------------------------------------
-
-		//Ignore rx until break is received
-
-	}
-	else
-	{
-		//-------------------------
-		//----- DATA RECEIVED -----
-		//-------------------------
-
-		//RESET THE TIMEOUT TIMER
-		dmx_rx_10mS_timeout_timer = DMX_TIMEOUT_10MS_TIME;
-
-		//GET THE DATA BYTE IF IN OUR CHANNEL RANGE
-		if (
-			(dmx_rx_channel_number >= dmx_start_address) &&
-			(dmx_rx_channel_number < (dmx_start_address + DMX_NO_OF_CHANS_TO_RX))
-			)
-		{
-			//THIS CHANNEL IS IN OUR RX WINDOW
-			dmx_rx[dmx_rx_channel_number - dmx_start_address] = rx_data;
-		}
-
-		//FLAG NEW FRAME RECEVIED IF WE HAVE RECEIVED ALL THE CHANNELS WE WANT TO RECEIVE
-		if (dmx_rx_channel_number == (dmx_start_address + DMX_NO_OF_CHANS_TO_RX - 1))
-			dmx_new_frame_received = 1;
-
-		//INCREMENT THE CHANNEL NUMBER READY FOR THE NEXT BYTE
-		dmx_rx_channel_number++;
-		if (dmx_rx_channel_number > 512)
-		{
-			waiting_for_break = 1;
-		}
-
-	}
-
-
+	} //while (PIE3bits.RC2IE && PIR3bits.RC2IF)
 
 }
+
+
 
 //***************************************
 //***************************************
